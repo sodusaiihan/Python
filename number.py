6ee3d5c77daa2f5084e87395a4e9b886cdcbca4b
Sodu <m.sodoo9911@gmail.com>
2021-11-06 13:35:16 +0800

    chapter-1 ending

---
+++ number.py
@@ -0,0 +1,29 @@
+# integers
+
+add = 9 + 9
+subtract = 18 - 9
+multiply = 9 * 9
+divide = 18 / 9
+
+
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+
+# floats
+
+universe_age = 14_000_000_00
+print(universe_age)
+
+add = 4 + 4
+subtract = 16 - 8
+multiply = 4 * 2
+divide = 16 / 2
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+
+АЗЫН_ТОО = 11
+print(f"Миний азын тоо {АЗЫН_ТОО}")
