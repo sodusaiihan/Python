2f51c415cd3c8722d832f0df44c260d0eb2e816c
Sodu <m.sodoo9911@gmail.com>
2021-11-07 11:19:34 +0800

    Organizing folder and ended chapter-3

---
+++ chapter_2/name_cases.py
@@ -0,0 +1,18 @@
+person = 'насанжаргал'
+print(f"Сайн уу, {person.title()}-аа, Would you like to learn some Python today?")
+print(person.lower())
+print(person.upper())
+print(person.title())
+
+quotes = "A person who never made a mistake never tried anything new"
+famous_person = "Albert Einstein"
+print(f"{famous_person} once said, '{quotes}.'")
+
+quotes = "Хүн аливаа зүйлд зүтгэл гаргаж байж л түүнийхээ жаргал, зовлон хоёрыг нь таньдаг юм. Жаргалтай болмоор байвал зүтгэж, жаргалыг олтлоо зовдог."
+famous_person = "Нэгүдэ Цагаан Ов А"
+print(f"{famous_person}, '{quotes}'")
+
+crush = " Насанжаргал "
+print(crush.rstrip())
+print(crush.lstrip())
+print(crush.strip())
